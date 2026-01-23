4c36aa7a9a7c0cc74e28e004365dea1f88cbe80f
Martin Terziev <m.terziev08@abv.bg>
2026-01-23 22:49:38 +0200

    Added ApplicationUser, Order and ProductSale entities.

---
+++ TechHaven/Data/Models/Order.cs
@@ -0,0 +1,10 @@
+﻿namespace TechHaven.Data.Models;
+
+public class Order
+{
+    public Guid Id { get; set; }
+
+    public DateTime OrderDate { get; set; }
+
+    public virtual ICollection<ProductSale> OrderItems { get; set; } = new List<ProductSale>();
+}
---
+++ TechHaven/Data/Models/ProductSale.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TechHaven.Data.Models;
+
+public class ProductSale
+{
+    [ForeignKey(nameof(Order))]
+    public Guid OrderId { get; set; }
+
+    public virtual Order Order { get; set; } = null!;
+
+    [ForeignKey(nameof(Product))]
+    public int ProductId { get; set; }
+
+    public virtual Product Product { get; set; } = null!;
+
+    public int Quantity { get; set; }
+}
