fe78f100b8abbc61f96ab500d2dd28d6d778f693
Martin Terziev <m.terziev08@abv.bg>
2026-01-23 22:08:00 +0200

    Created Category entity.

---
+++ TechHaven/Data/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TechHaven.Data.Models;
+
+public class Category
+{
+    [Key]
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+}
