afc45d4767a532b0fb70afba446fe782ace0e07a
Martin Terziev <m.terziev08@abv.bg>
2026-01-23 22:01:30 +0200

    Created Entity Validation class.

---
+++ TechHaven/Common/EntityValidation.cs
@@ -0,0 +1,12 @@
+﻿namespace TechHaven.Common;
+
+public static class EntityValidation
+{
+    public static class Product
+    {
+        public const int NameMaxLength = 100;
+        public const int DescriptionMaxLength = 1000;
+        public const int SpecsJsonMaxLength = 2000;
+        public const int ImageUrlMaxLength = 500;
+    }
+}
