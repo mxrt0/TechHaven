117f1403dd68a4efa37e7752adfd69c6008233ef
Martin Terziev <m.terziev08@abv.bg>
2026-01-24 16:54:56 +0200

    Added User relationship to Order entity.

--- TechHaven/Data/Models/Order.cs
+++ TechHaven/Data/Models/Order.cs
@@ -1,8 +1,14 @@
-﻿namespace TechHaven.Data.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TechHaven.Data.Models;
 
 public class Order
 {
     public Guid Id { get; set; }
+
+    [Required]
+    public string UserId { get; set; } = null!;
+    public ApplicationUser User { get; set; } = null!;
 
     public DateTime OrderDate { get; set; }
 
