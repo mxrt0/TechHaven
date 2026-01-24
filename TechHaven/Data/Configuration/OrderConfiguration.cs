e901ece8d73c0bc031f1c0038ea58f8b0a798f67
Martin Terziev <m.terziev08@abv.bg>
2026-01-25 01:34:56 +0200

    Created config file for Order entity to explicitly set table name.

---
+++ TechHaven/Data/Configuration/OrderConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TechHaven.Data.Models;
+
+namespace TechHaven.Data.Configuration;
+
+public class OrderConfiguration : IEntityTypeConfiguration<Order>
+{
+    public void Configure(EntityTypeBuilder<Order> builder)
+    {
+        builder.ToTable("Orders");
+    }
+}
