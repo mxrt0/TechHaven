c8df5ee6ea0a7f63ee789f9ff14548d12d657918
Martin Terziev <m.terziev08@abv.bg>
2026-01-24 17:13:00 +0200

    Added WishlistItem entity.

---
+++ TechHaven/Data/Models/WishlistItem.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TechHaven.Data.Models;
+
+public class WishlistItem
+{
+    public int Id { get; set; }
+
+    [Required]
+    [ForeignKey(nameof(User))]
+    public string UserId { get; set; } = null!;
+    public ApplicationUser User { get; set; } = null!;
+
+    [ForeignKey(nameof(Product))]
+    public int ProductId { get; set; }
+    public Product Product { get; set; } = null!;
+
+    public DateTime AddedAt { get; set; }
+}
