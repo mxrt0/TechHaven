2a25fe74ea317f8d28e6734c30285abdfce7709c
Martin Terziev <m.terziev08@abv.bg>
2026-01-24 16:50:41 +0200

    Configured DbSet properties

--- TechHaven/Data/ApplicationDbContext.cs
+++ TechHaven/Data/ApplicationDbContext.cs
@@ -1,13 +1,20 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using TechHaven.Data.Models;
 
 namespace TechHaven.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+        public virtual DbSet<Order> Categories { get; set; }
+        public virtual DbSet<ProductSale> ProductSales { get; set; }
+
+
     }
 }
