8b0f663b67b752647c87b0cad79c5a047a2b061f
Martin Terziev <m.terziev08@abv.bg>
2026-01-25 01:11:36 +0200

    Added ImageUrls to seeded categories.

--- TechHaven/Data/Configuration/CategoryConfiguration.cs
+++ TechHaven/Data/Configuration/CategoryConfiguration.cs
@@ -8,25 +8,25 @@ public class CategoryConfiguration : IEntityTypeConfiguration<Category>
 {
     private readonly Category[] categories = [
 
-    new Category { Id = 1, Name = "CPU" },
+    new Category { Id = 1, Name = "CPU", ImageUrl = "/images/categories/cpu.jpg" },
 
-    new Category { Id = 2, Name = "GPU" },
+    new Category { Id = 2, Name = "GPU", ImageUrl = "/images/categories/gpu.jpg" },
 
-    new Category { Id = 3, Name = "Motherboard" },
+    new Category { Id = 3, Name = "Motherboard", ImageUrl = "/images/categories/motherboard.jpg" },
 
-    new Category { Id = 4, Name = "RAM" },
+    new Category { Id = 4, Name = "RAM", ImageUrl = "/images/categories/ram.jpg" },
 
-    new Category { Id = 5, Name = "Storage" },
+    new Category { Id = 5, Name = "Storage", ImageUrl = "/images/categories/storage.jpg" },
 
-    new Category { Id = 6, Name = "Power Supply" },
+    new Category { Id = 6, Name = "Power Supply", ImageUrl = "/images/categories/psu.jpg"  },
 
-    new Category { Id = 7, Name = "Cooling" },
+    new Category { Id = 7, Name = "Cooling", ImageUrl = "/images/categories/cooling.jpg"  },
 
-    new Category { Id = 8, Name = "Case" },
+    new Category { Id = 8, Name = "Case", ImageUrl = "/images/categories/case.jpg"  },
 
-    new Category {Id = 9, Name = "Peripheral"},
+    new Category {Id = 9, Name = "Peripheral", ImageUrl = "/images/categories/peripheral.jpg" },
 
-    new Category {Id = 10, Name = "Monitor"}
+    new Category {Id = 10, Name = "Monitor", ImageUrl = "/images/categories/monitor.jpg" }
 
     ];
     public void Configure(EntityTypeBuilder<Category> builder)
