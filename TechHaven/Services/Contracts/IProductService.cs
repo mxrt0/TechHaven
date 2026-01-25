401d9657fd758f2c02fbe075f0227759277f23bf
Martin Terziev <m.terziev08@abv.bg>
2026-01-25 11:56:04 +0200

    Added methods for querying products in product service contract.

--- TechHaven/Services/Contracts/IProductService.cs
+++ TechHaven/Services/Contracts/IProductService.cs
@@ -1,5 +1,10 @@
-﻿namespace TechHaven.Services.Contracts;
+﻿using TechHaven.DTOs.Products;
+
+namespace TechHaven.Services.Contracts;
 
 public interface IProductService
 {
+    Task<IReadOnlyList<ProductListDto>> GetAllAsync();
+    Task<IReadOnlyList<ProductListDto>> GetByCategoryAsync(int categoryId);
+    Task<ProductDetailsDto?> GetByIdAsync(int id);
 }
