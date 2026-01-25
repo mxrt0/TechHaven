81e1641be9502bf9a4554239ec8141cd67f583bb
Martin Terziev <m.terziev08@abv.bg>
2026-01-25 18:41:58 +0200

    Injected service into product controller and implemented main actions.

--- TechHaven/Controllers/ProductsController.cs
+++ TechHaven/Controllers/ProductsController.cs
@@ -1,11 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TechHaven.Services.Contracts;
 
 namespace TechHaven.Controllers;
 
 public class ProductsController : Controller
 {
-    public IActionResult Index()
+    private readonly IProductService _productService;
+    public ProductsController(IProductService productService)
     {
-        return View();
+        _productService = productService;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var products = await _productService.GetAllAsync();
+        return View(products);
+    }
+
+    public async Task<IActionResult> Category(int id)
+    {
+        var products = await _productService.GetByCategoryAsync(id);
+        return View(nameof(Index), products);
+    }
+    public async Task<IActionResult> Details(int id)
+    {
+        var product = await _productService.GetByIdAsync(id);
+        return product is null ? NotFound() : View(product);
     }
 }
