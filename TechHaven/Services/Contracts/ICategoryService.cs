a922a791ebeadba236ea50f2eb7b428ecdc820d7
Martin Terziev <m.terziev08@abv.bg>
2026-01-25 19:27:56 +0200

    Added basic contract methods for querying categories.

--- TechHaven/Services/Contracts/ICategoryService.cs
+++ TechHaven/Services/Contracts/ICategoryService.cs
@@ -1,5 +1,9 @@
-﻿namespace TechHaven.Services.Contracts;
+﻿using TechHaven.DTOs.Categories;
+
+namespace TechHaven.Services.Contracts;
 
 public interface ICategoryService
 {
+    Task<IReadOnlyList<CategoryListDto>> GetAllAsync();
+    Task<CategoryListDto?> GetByIdAsync(int id);
 }
