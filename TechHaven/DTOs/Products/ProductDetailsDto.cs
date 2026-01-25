56a91acf2d6aa9f0e5210e16595a4e0247547eb2
Martin Terziev <m.terziev08@abv.bg>
2026-01-25 10:58:09 +0200

    Added DTOs for creating and querying Products.

---
+++ TechHaven/DTOs/Products/ProductDetailsDto.cs
@@ -0,0 +1,13 @@
+﻿namespace TechHaven.DTOs.Products;
+
+public sealed record ProductDetailsDto(
+    int Id,
+    string Name,
+    string Description,
+    string SpecsJson,
+    decimal Price,
+    int StockQuantity,
+    string ImageUrl,
+    string Category
+);
+
