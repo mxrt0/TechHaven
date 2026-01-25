35ce96ba088b5ce932d153b0b14c21b71c54682b
Martin Terziev <m.terziev08@abv.bg>
2026-01-25 17:59:37 +0200

    Added products controller.

---
+++ TechHaven/Controllers/ProductsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TechHaven.Controllers;
+
+public class ProductsController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
