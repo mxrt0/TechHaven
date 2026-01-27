cf97976635cf0d40bd2b236fa23079d8afe06396
Martin Terziev <m.terziev08@abv.bg>
2026-01-27 20:10:50 +0200

    Created wishlist service and implemented contract methods.

---
+++ TechHaven/Services/WishlistService.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+using TechHaven.Data;
+using TechHaven.Data.Models;
+using TechHaven.Services.Contracts;
+
+namespace TechHaven.Services;
+
+public class WishlistService : IWishlistService
+{
+    private readonly ApplicationDbContext _context;
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public WishlistService(
+        ApplicationDbContext context,
+        UserManager<ApplicationUser> userManager)
+    {
+        _context = context;
+        _userManager = userManager;
+    }
+
+    public async Task<bool> IsInWishlistAsync(int productId, ClaimsPrincipal principal)
+    {
+        var userId = _userManager.GetUserId(principal);
+        var item = await _context.WishlistItems
+            .FindAsync(userId, productId);
+        return item is not null;
+    }
+
+    public async Task<bool> ToggleAsync(int productId, ClaimsPrincipal principal)
+    {
+        var userId = _userManager.GetUserId(principal);
+
+        var item = await _context.WishlistItems
+            .FindAsync(userId, productId);
+
+        bool added;
+        if (item is null)
+        {
+            _context.WishlistItems.Add(new WishlistItem
+            {
+                UserId = userId!,
+                ProductId = productId
+            });
+            added = true;
+        }
+        else
+        {
+            _context.WishlistItems.Remove(item);
+            added = false;
+        }
+
+        await _context.SaveChangesAsync();
+        return added;
+    }
+}
