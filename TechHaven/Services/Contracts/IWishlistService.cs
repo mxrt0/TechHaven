7d203c05c7328a976afc9b7434a6300cefd70394
Martin Terziev <m.terziev08@abv.bg>
2026-01-27 20:10:09 +0200

    Added methods to wishlist service contract.

--- TechHaven/Services/Contracts/IWishlistService.cs
+++ TechHaven/Services/Contracts/IWishlistService.cs
@@ -1,5 +1,9 @@
-﻿namespace TechHaven.Services.Contracts;
+﻿using System.Security.Claims;
+
+namespace TechHaven.Services.Contracts;
 
 public interface IWishlistService
 {
+    Task<bool> ToggleAsync(int productId, ClaimsPrincipal principal);
+    Task<bool> IsInWishlistAsync(int productId, ClaimsPrincipal principal);
 }
