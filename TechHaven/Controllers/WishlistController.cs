735f54a13e7d9b3bf426a16b0217c2388fc61323
Martin Terziev <m.terziev08@abv.bg>
2026-01-27 20:11:20 +0200

    Added wishlist controller with a wishlist toggle action.

---
+++ TechHaven/Controllers/WishlistController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TechHaven.Common;
+using TechHaven.Services.Contracts;
+
+namespace TechHaven.Controllers;
+
+
+public class WishlistController : Controller
+{
+    private readonly IWishlistService _wishlistService;
+    public WishlistController(IWishlistService wishlistService)
+    {
+        _wishlistService = wishlistService;
+    }
+
+    [Authorize]
+    [HttpPost]
+    public async Task<IActionResult> Toggle(int productId)
+    {
+        bool added = await _wishlistService.ToggleAsync(productId, User);
+        return Json(new
+        {
+            added,
+            message = added
+                    ? Messages.AddedToWishlistMessage
+                    : Messages.RemovedFromWishlistMessage
+        });
+    }
+
+}
