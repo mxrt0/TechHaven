293a55ccbcbe31146ad90d87a1c87dff5e8b442b
Martin Terziev <m.terziev08@abv.bg>
2026-01-31 15:57:53 +0200

    Added cart service contract with basic methods.

---
+++ TechHaven/Services/Contracts/ICartService.cs
@@ -0,0 +1,12 @@
+﻿using TechHaven.DTOs.Cart;
+
+namespace TechHaven.Services.Contracts;
+
+public interface ICartService
+{
+    void Add(int productId, int quantity = 1);
+    void Remove(int productId);
+    void UpdateQuantity(int productId, int quantity);
+    IEnumerable<CartItemDto> GetCart();
+    void Clear();
+}
