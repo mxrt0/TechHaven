270b29d34bf3880e934b568a0954ae813cabd95d
Martin Terziev <m.terziev08@abv.bg>
2026-01-31 16:07:15 +0200

    Made add, remove and update cart contract methods asynchronous.

--- TechHaven/Services/Contracts/ICartService.cs
+++ TechHaven/Services/Contracts/ICartService.cs
@@ -4,9 +4,9 @@ namespace TechHaven.Services.Contracts;
 
 public interface ICartService
 {
-    void Add(int productId, int quantity = 1);
-    void Remove(int productId);
-    void UpdateQuantity(int productId, int quantity);
+    Task Add(int productId, int quantity = 1);
+    Task Remove(int productId);
+    Task UpdateQuantity(int productId, int quantity);
     IEnumerable<CartItemDto> GetCart();
     void Clear();
 }
