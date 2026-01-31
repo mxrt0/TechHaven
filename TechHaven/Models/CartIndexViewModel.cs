8ee84b20d2d9e8a4ee02d45a21ae8dd22056b6de
Martin Terziev <m.terziev08@abv.bg>
2026-01-31 16:52:46 +0200

    Added cart index VM for displaying cart items in View.

---
+++ TechHaven/Models/CartIndexViewModel.cs
@@ -0,0 +1,9 @@
+﻿using TechHaven.DTOs.Cart;
+
+namespace TechHaven.Models;
+
+public class CartIndexViewModel
+{
+    public IEnumerable<CartItemDto> Items { get; set; } = new List<CartItemDto>();
+    public decimal TotalPrice { get; set; }
+}
