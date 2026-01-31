708eabe572c41742ef868ff354aebc3bb6e5f8cc
Martin Terziev <m.terziev08@abv.bg>
2026-01-31 20:37:01 +0200

    Configured wishlist script to match extracted function.

--- TechHaven/wwwroot/ts/wishlist.ts
+++ TechHaven/wwwroot/ts/wishlist.ts
@@ -1,5 +1,4 @@
-﻿/// <reference types="bootstrap" />
-type ToggleJsonResponse = {
+﻿type ToggleJsonResponse = {
     added: boolean,
     message: string
 };
@@ -46,7 +45,10 @@ wishlistBtns.forEach(wishlistBtn => {
         }
         icon?.classList.add('toggle-animation');
         setTimeout(() => icon?.classList.remove('toggle-animation'), 300);
-        showWishlistToast(result.message, result.added);
+        showToast(result.message, {
+            type: ToastType.Wishlist,
+            added: result.added
+        });
 
         wishlistBtn.disabled = false;
 
@@ -61,30 +63,3 @@ wishlistBtns.forEach(wishlistBtn => {
         }
     });
 })
-
-
-function showWishlistToast(message: string, added: boolean = true) {
-    const toastEl = document.getElementById('wishlistToast') as HTMLElement;
-    const body = document.getElementById('wishlistToastBody') as HTMLElement;
-    const timer = toastEl.querySelector('.neon-timer') as HTMLElement;
-
-    body.textContent = message;
-
-    timer.style.transition = "none";
-    timer.style.width = "100%";
-
-    void timer.offsetWidth;
-
-    timer.style.transition = `width 2ms linear`;
-    timer.style.width = "0%";
-
-    toastEl.style.borderColor = added ? "#0ff" : "#f43f5e";
-    toastEl.style.color = added ? "#0ff" : "#f43f5e";
-    toastEl.style.textShadow = added
-        ? "0 0 2px #0ff, 0 0 5px #0ff, 0 0 10px #0ff"
-        : "0 0 2px #f43f5e, 0 0 4px #f43f5e, 0 0 6px #f43f5e";
-
-
-    const toast = new bootstrap.Toast(toastEl, { delay: 2000 });
-    toast.show();
-}
