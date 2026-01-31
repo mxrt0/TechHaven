9dc3fa37256a30bf2faebb04e69f760af872c9e5
Martin Terziev <m.terziev08@abv.bg>
2026-01-31 16:32:12 +0200

    Made contract method for retrieving cart contents nullable in the event of an empty cart

--- TechHaven/Services/Contracts/ICartService.cs
+++ TechHaven/Services/Contracts/ICartService.cs
@@ -7,6 +7,6 @@ public interface ICartService
     void Add(int productId, int quantity = 1);
     void Remove(int productId);
     void UpdateQuantity(int productId, int quantity);
-    IEnumerable<CartItemDto> GetCart();
+    IEnumerable<CartItemDto>? GetCart();
     void Clear();
 }
