a9da52fffafccd3887d1744b1cf07cbfa033ee51
Martin Terziev <m.terziev08@abv.bg>
2026-02-03 19:53:00 +0200

    Added basic methods in the order service contract.

--- TechHaven/Services/Contracts/IOrderService.cs
+++ TechHaven/Services/Contracts/IOrderService.cs
@@ -1,5 +1,14 @@
-﻿namespace TechHaven.Services.Contracts;
+﻿using System.Security.Claims;
+using TechHaven.Data.Models;
+using TechHaven.DTOs.Order;
+
+namespace TechHaven.Services.Contracts;
 
 public interface IOrderService
 {
+    Task<bool> CreateOrderAsync(IEnumerable<OrderItemDto> orderItems, ClaimsPrincipal principal);
+
+    Task<IEnumerable<OrderListDto>> GetOrdersByUserIdAsync(ClaimsPrincipal principal);
+    Task<OrderListDto?> GetOrderByIdAsync(Guid orderId);
+    Task<bool> CancelOrderAsync(Guid orderId, ClaimsPrincipal principal);
 }
