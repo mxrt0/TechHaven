8caa1008194603b85fa23f66fd962aad113ba058
Martin Terziev <m.terziev08@abv.bg>
2026-02-04 21:25:04 +0200

    Added view models for order views.

---
+++ TechHaven/Models/OrderDetailsViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TechHaven.DTOs.Order;
+
+namespace TechHaven.Models;
+
+public class OrderDetailsViewModel
+{
+    public OrderListDto Order { get; set; } = null!;
+}
---
+++ TechHaven/Models/OrderIndexViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TechHaven.DTOs.Order;
+
+namespace TechHaven.Models;
+
+public class OrderIndexViewModel
+{
+    public IEnumerable<OrderListDto> Orders { get; set; } = new List<OrderListDto>();
+}
