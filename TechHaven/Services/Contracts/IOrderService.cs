a5cc24e3dbe86f1b3ac459a2346eb2d593914eea
Martin Terziev <m.terziev08@abv.bg>
2026-02-05 16:04:22 +0200

    Changed create order method to take cart item dto in contract.

--- TechHaven/Services/Contracts/IOrderService.cs
+++ TechHaven/Services/Contracts/IOrderService.cs
@@ -1,12 +1,12 @@
 ﻿using System.Security.Claims;
-using TechHaven.Data.Models;
 using TechHaven.DTOs.Order;
+using TechHaven.DTOs.Cart;
 
 namespace TechHaven.Services.Contracts;
 
 public interface IOrderService
 {
-    Task<bool> CreateOrderAsync(IEnumerable<OrderItemDto> orderItems, ClaimsPrincipal principal);
+    Task<bool> CreateOrderAsync(IEnumerable<CartItemDto> orderItems, ClaimsPrincipal principal);
 
     Task<IEnumerable<OrderListDto>> GetOrdersByUserIdAsync(ClaimsPrincipal principal);
     Task<OrderListDto?> GetOrderByIdAsync(Guid orderId, ClaimsPrincipal principal);
