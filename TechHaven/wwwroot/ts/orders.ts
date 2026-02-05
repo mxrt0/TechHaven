4d1653471e295b368b053cd8f4bd11f32559a9dd
Martin Terziev <m.terziev08@abv.bg>
2026-02-05 19:38:53 +0200

    Added script to dynamically update modal form action URL.

--- TechHaven/wwwroot/ts/orders.ts
+++ TechHaven/wwwroot/ts/orders.ts
@@ -6,3 +6,17 @@ if (tempMsg) {
         delay: 1800
     });
 };
+
+const modal = document.getElementById('cancelModal');
+const modalForm = document.getElementById('cancelModalForm') as HTMLFormElement;
+
+modal?.addEventListener('show.bs.modal', (event: any) => {
+    const button = event.relatedTarget as HTMLButtonElement | null;
+    if (!button || !modalForm) return;
+
+    const orderId = button.dataset.orderId;
+    if (orderId) {
+        modalForm.action = `/Orders/Cancel/${orderId}`;
+    }
+});
+
