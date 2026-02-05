08e4e7e61f0e1068ac9b9f8b6c35fc95ef4827d3
Martin Terziev <m.terziev08@abv.bg>
2026-02-05 16:23:08 +0200

    Created simple orders script for displaying temp data message in a toast.

---
+++ TechHaven/wwwroot/ts/orders.ts
@@ -0,0 +1,8 @@
+﻿const tempMsgContainer = document.getElementById('temp-message');
+const tempMsg = tempMsgContainer?.dataset.tempMsg;
+if (tempMsg) {
+    showToast(tempMsg, {
+        type: ToastType.Order,
+        delay: 1800
+    });
+};
