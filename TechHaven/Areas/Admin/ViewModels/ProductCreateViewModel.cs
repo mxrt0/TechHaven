4775d1832d9ea22bbcc6497555ad4cdf5bc1c72b
Martin Terziev <m.terziev08@abv.bg>
2026-02-07 18:52:46 +0200

    Added viewmodel for creating a product.

---
+++ TechHaven/Areas/Admin/ViewModels/ProductCreateViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using TechHaven.DTOs.Admin.Products;
+
+namespace TechHaven.Areas.Admin.ViewModels;
+
+public class ProductCreateViewModel
+{
+    public SelectList Categories { get; set; } = null!; 
+    public AdminProductCreateDto Product { get; set; } = null!;
+}
