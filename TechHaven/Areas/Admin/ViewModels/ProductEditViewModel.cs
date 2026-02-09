aeef2b5d2eaca4055aedcc37dd890ee60890e983
Martin Terziev <m.terziev08@abv.bg>
2026-02-09 20:11:51 +0200

    Added product edit vm

---
+++ TechHaven/Areas/Admin/ViewModels/ProductEditViewModel.cs
@@ -0,0 +1,7 @@
+﻿using TechHaven.DTOs.Admin.Products;
+namespace TechHaven.Areas.Admin.ViewModels;
+
+public class ProductEditViewModel
+{
+    public AdminProductEditDto Product { get; set; } = null!;
+}
