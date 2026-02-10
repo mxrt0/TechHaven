6034dd10014561df9c7517dd7b58b562a782c84c
Martin Terziev <m.terziev08@abv.bg>
2026-02-10 17:13:56 +0200

    Added products index view model.

---
+++ TechHaven/Models/ProductsIndexViewModel.cs
@@ -0,0 +1,18 @@
+﻿using TechHaven.DTOs.Public.Categories;
+using TechHaven.DTOs.Public.Products;
+
+namespace TechHaven.Models;
+
+public class ProductsIndexViewModel
+{
+    public IReadOnlyList<ProductListDto> Products { get; set; } 
+        = new List<ProductListDto>();
+
+    public IReadOnlyList<CategoryListDto> Categories { get; set; }
+       = new List<CategoryListDto>();
+
+    public string? SearchTerm { get; set; }
+    public int? CategoryId { get; set; }
+    public decimal? MinPrice { get; set; }
+    public decimal? MaxPrice { get; set; }
+}
