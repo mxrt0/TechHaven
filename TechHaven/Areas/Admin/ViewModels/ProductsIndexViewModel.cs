c065043dc73866a067c569aa725a8e6488bb73ac
Martin Terziev <m.terziev08@abv.bg>
2026-02-11 17:37:24 +0200

    Added search/filter-purposed properties to Admin Products Index VM

--- TechHaven/Areas/Admin/ViewModels/ProductsIndexViewModel.cs
+++ TechHaven/Areas/Admin/ViewModels/ProductsIndexViewModel.cs
@@ -1,8 +1,20 @@
-﻿using TechHaven.DTOs.Admin.Products;
+﻿using TechHaven.Areas.Admin.ViewModels.Enums;
+using TechHaven.DTOs.Admin.Products;
+using TechHaven.DTOs.Public.Categories;
 
 namespace TechHaven.Areas.Admin.ViewModels;
 
 public class ProductsIndexViewModel
 {
+    public string? SearchTerm { get; set; }
+
+    public int? CategoryId { get; set; }
+
+    public StockFilter Stock { get; set; } = StockFilter.All;
+
+    public ProductSort SortBy { get; set; } = ProductSort.Newest;
+
+    public IEnumerable<CategoryListDto> Categories { get; set; } = new List<CategoryListDto>();
+
     public IEnumerable<AdminProductListDto> Products { get; set; } = null!;
 }
