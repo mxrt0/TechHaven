2efa305f57d8702be33f7d11550c0e8e76dcb53a
Martin Terziev <m.terziev08@abv.bg>
2026-02-13 21:50:13 +0200

    Added status in order list dto.

--- TechHaven/DTOs/Public/Order/OrderListDto.cs
+++ TechHaven/DTOs/Public/Order/OrderListDto.cs
@@ -1,9 +1,12 @@
-﻿namespace TechHaven.DTOs.Public.Order;
+﻿using TechHaven.Data.Enums;
+
+namespace TechHaven.DTOs.Public.Order;
 
 public sealed record OrderListDto(
 Guid OrderId,
 string OrderNumber,
 DateTime OrderDate,
+OrderStatus Status,
 List<ProductSaleListDto> OrderItems
 )
 {
