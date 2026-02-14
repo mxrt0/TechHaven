54e51ec7077f85ecf0b40fbe8130b32402b63472
Martin Terziev <m.terziev08@abv.bg>
2026-02-14 15:32:04 +0200

    Added global exception handler.

--- TechHaven/Program.cs
+++ TechHaven/Program.cs
@@ -73,11 +73,11 @@ namespace TechHaven
                 app.UseMigrationsEndPoint();
             }
             else
-            {
-                app.UseExceptionHandler("/Home/Error");
+            {               
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.UseExceptionHandler("/Home/Error");
 
             app.UseHttpsRedirection();
             app.UseRouting();
