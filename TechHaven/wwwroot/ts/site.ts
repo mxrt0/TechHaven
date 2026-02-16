6adb83677ffecf0d75b42e3e6afca16e5a047c62
Martin Terziev <m.terziev08@abv.bg>
2026-02-16 17:30:49 +0200

    Added logic for adding close button to every error message handler div.

--- TechHaven/wwwroot/ts/site.ts
+++ TechHaven/wwwroot/ts/site.ts
@@ -1,1 +1,16 @@
 ﻿/// <reference types="bootstrap" />
+
+const errorDiv = document.querySelector('div.alert');
+
+if (errorDiv) {
+    const closeBtn = document.createElement('button');
+    closeBtn.innerHTML = '&times;';
+    closeBtn.className = 'close-btn';
+
+
+    closeBtn.onclick = function () {
+        errorDiv.remove();
+    };
+
+    errorDiv.appendChild(closeBtn);
+}
